2f381d1da4b764df782ad1f1aab54ebe9ad6b251
dottg0n20 <dev@dariocaruso.info>
2021-01-26 19:02:51 +0100

    better chat

--- bin/telesend.js
+++ bin/telesend.js
@@ -9,7 +9,12 @@ const os_1 = __importDefault(require("os"));
 const path_1 = __importDefault(require("path"));
 const __1 = __importDefault(require(".."));
 // console.log(process.argv)
-console.log('--- START SEND TELEGRAM MESSAGE ---');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let lastArgv;
 try {
     const appConfigFile = path_1.default.join(os_1.default.homedir(), '.telegrammessagebot');
     let config;
@@ -28,24 +33,36 @@ try {
         throw new Error('botId not configured');
     if (!config.currentChatId)
         throw new Error('chatId not configured');
-    const lastArgv = process.argv.filter(f => !f.includes('/'));
-    const text = '[' + os_1.default.userInfo().username + '@' + os_1.default.hostname + ']: ' + lastArgv[lastArgv.length - 1];
-    const botId = config.botId;
-    const chatId = config.currentChatId;
-    console.log('text: ' + text);
-    console.log('botId: ' + botId);
-    console.log('chatId: ' + chatId);
-    console.log('---- sending message...');
-    __1.default({ text, botId, chatId })
-        .then(data => {
-        console.log('message sent!');
-        console.log('response:', data);
-        console.log('--- END SEND TELEGRAM MESSAGE OK ---');
-    })
-        .catch(err => {
-        console.error('error on sending message!!!');
+    const currentChat = config.chats.find(f => f.chatId === config.currentChatId);
+    rl.question('(' + currentChat.label + ') TEXT: ', function (txt) {
+        console.log('--- START SEND TELEGRAM MESSAGE ---');
+        lastArgv = txt;
+        const text = '[' + os_1.default.userInfo().username + '@' + os_1.default.hostname + ']: ' + lastArgv;
+        const botId = config.botId;
+        const chatId = config.currentChatId;
+        console.log('text: ' + text);
+        console.log('botId: ' + botId);
+        console.log('chatId: ' + chatId);
+        __1.default({ text, botId, chatId })
+            .then(data => {
+            console.log('message sent!');
+            console.log('response:', data);
+            console.log('--- END SEND TELEGRAM MESSAGE OK ---');
+        })
+            .catch(err => {
+            console.error('error on sending message!!!');
+            console.error(err);
+            console.log('--- END SEND TELEGRAM MESSAGE ERROR ---');
+        });
+        rl.close();
+    });
+    rl.on('error', function (err) {
         console.error(err);
-        console.log('--- END SEND TELEGRAM MESSAGE ERROR ---');
+        process.exit(0);
+    });
+    rl.on('close', function () {
+        console.log('\nBYE BYE !!!');
+        process.exit(0);
     });
 }
 catch (err) {
--- bin/telesend.ts
+++ bin/telesend.ts
@@ -7,14 +7,21 @@ import sendTelegramMessage from '..'
 
 // console.log(process.argv)
 
-console.log('--- START SEND TELEGRAM MESSAGE ---')
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+let lastArgv: string
+
 
 try {
   const appConfigFile = path.join(os.homedir(), '.telegrammessagebot')
   let config: {
     currentChatId: number
     botId: string
-    chats: string[]
+    chats: { label: string; chatId: number }[]
   }
   try {
     config = JSON.parse(fs.readFileSync(appConfigFile, 'utf-8'))
@@ -30,31 +37,49 @@ try {
   if (!config.botId) throw new Error('botId not configured')
   if (!config.currentChatId) throw new Error('chatId not configured')
 
-  const lastArgv = process.argv.filter(f => !f.includes('/'))
-  const text = '[' + os.userInfo().username + '@'+os.hostname+']: ' + lastArgv[lastArgv.length - 1]
-  const botId = config.botId
-  const chatId = config.currentChatId
+  const currentChat = config.chats.find(f => f.chatId === config.currentChatId)
 
-  console.log('text: ' + text)
-  console.log('botId: ' + botId)
-  console.log('chatId: ' + chatId)
+  rl.question('(' + currentChat.label + ') TEXT: ', function (txt) {
+    console.log('--- START SEND TELEGRAM MESSAGE ---')
 
-  console.log('---- sending message...')
+    lastArgv = txt
 
-  sendTelegramMessage({ text, botId, chatId })
-    .then(data => {
-      console.log('message sent!')
-      console.log('response:', data)
+    const text = '[' + os.userInfo().username + '@' + os.hostname + ']: ' + lastArgv
+    const botId = config.botId
+    const chatId = config.currentChatId
 
-      console.log('--- END SEND TELEGRAM MESSAGE OK ---')
-    })
-    .catch(err => {
-      console.error('error on sending message!!!')
+    console.log('text: ' + text)
+    console.log('botId: ' + botId)
+    console.log('chatId: ' + chatId)
 
-      console.error(err)
 
-      console.log('--- END SEND TELEGRAM MESSAGE ERROR ---')
-    })
+    sendTelegramMessage({ text, botId, chatId })
+      .then(data => {
+        console.log('message sent!')
+        console.log('response:', data)
+
+        console.log('--- END SEND TELEGRAM MESSAGE OK ---')
+      })
+      .catch(err => {
+        console.error('error on sending message!!!')
+
+        console.error(err)
+
+        console.log('--- END SEND TELEGRAM MESSAGE ERROR ---')
+      })
+
+    rl.close()
+  })
+
+  rl.on('error', function (err) {
+    console.error(err)
+    process.exit(0)
+  })
+
+  rl.on('close', function () {
+    console.log('\nBYE BYE !!!')
+    process.exit(0)
+  })
 } catch (err) {
   console.error(err)
 
